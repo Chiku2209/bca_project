b4057a88986f5d35d2b22cec2a32b19fb9ee21ee
Chiku2209 <namanvadhiya1647@gmail.com>
2023-06-28 21:44:34 +0530

    Some Changes

--- Admin/login_check.php
+++ Admin/login_check.php
@@ -6,10 +6,13 @@
 
     $qry = "SELECT * from users WHERE email='".$email."'&& password ='".md5($password)."'";
     $res = mysqli_query($conn, $qry);
+    $na = mysqli_fetch_assoc($res);
+    $username = $na['username'];
     $count = mysqli_num_rows($res);
 
     if($count>0){
         $_SESSION['email']=$email;
+        $_SESSION['name']=$username;
         header('location:dashboard.php');
     }else{
         header('location:index.php');
